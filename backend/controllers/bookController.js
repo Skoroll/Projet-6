14760ce71950044cfc53a53eb1e81d3dc04c1bfd
Yann <yg.gicquel@gmail.com>
2024-09-05 16:40:30 +0200

    Projet terminé. Reste à revoir les commentaire Focntionnel à 100%

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -4,33 +4,9 @@ const sharp = require("sharp");
 const path = require("path");
 
 
-const safeUnlink = async (filePath) => {
-  try {
-    await fs.unlink(filePath);
-    console.log(`Successfully deleted ${filePath}`);
-  } catch (error) {
-    if (error.code === "ENOENT") {
-      console.log(`File ${filePath} not found, nothing to delete.`);
-    } else {
-      console.error(`Failed to delete ${filePath}:`, error.message);
-    }
-  }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Délai avant de valider une opération
+//me permet de m'assurer que les opérations en cours se terminent avant de passer à la suite
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 //---------- Récupère tous les livres de la base de données
 exports.getAllBooks = async (req, res) => {
@@ -43,39 +19,69 @@ exports.getAllBooks = async (req, res) => {
   }
 };
 
-//---------- Permet de créer un nouveau livre
+//---------- Créer un nouveau livre
 exports.createBook = async (req, res) => {
   try {
-    console.log("Request received:", req.body);
-    console.log("Auth info:", req.auth);
+    console.log("Requête reçue:", req.body);
+    console.log("Informations d'authentification:", req.auth);
 
     if (!req.auth || !req.auth.userId) {
       return res.status(401).json({ message: "User not authenticated" });
     }
 
-    // Parse les données du livre
+    // Parse le livre envoyé dans le corps de la requête
     const bookObject = JSON.parse(req.body.book);
-    console.log("BookObject:", bookObject);
 
-    // Ajoute l'userId actuel
+    // Ajoute l'userId actuel au livre
     bookObject.userId = req.auth.userId;
 
-    // Crée l'URL de l'image après compression
     let imageUrl = null;
     if (req.file) {
-      const outputPath = `images/compressed-${req.file.filename}.webp`;
-      await sharp(req.file.path)
-        .resize(300) // Redimensionne si nécessaire
-        .webp({ quality: 80 }) // Compression WebP
-        .toFile(outputPath);
+      const originalFilePath = path.join(__dirname, "../images", req.file.filename);
+      const compressedFileName = `compressed-${req.file.filename}.webp`;
+      const compressedFilePath = path.join(__dirname, "../images", compressedFileName);
 
-      imageUrl = `${req.protocol}://${req.get("host")}/${outputPath}`;
+      console.log(`Début de la compression de l'image : ${originalFilePath}`);
 
-      // Supprime l'image originale non compressée
-      await safeUnlink(req.file.path);
+      try {
+        // Compression et conversion de l'image
+        await sharp(originalFilePath)
+          .resize(300) // Redimensionne l'image
+          .webp({ quality: 80 }) // Compression WebP
+          .toFile(compressedFilePath);
+        
+        console.log(`Compression terminée. Image sauvegardée : ${compressedFilePath}`);
+      } catch (compressionError) {
+        console.error(`Erreur lors de la compression de l'image : ${compressionError.message}`);
+        throw compressionError; // Rejette l'erreur pour qu'elle soit attrapée par le bloc catch principal
+      }
+
+      // Crée l'URL de l'image compressée
+      imageUrl = `${req.protocol}://${req.get("host")}/images/${compressedFileName}`;
+
+      // Ajoute un délai avant la suppression
+      console.log(`Tentative de suppression du fichier temporaire : ${originalFilePath}`);
+      setTimeout(async () => {
+        try {
+          await fs.unlink(originalFilePath);
+          console.log(`Fichier temporaire supprimé : ${originalFilePath}`);
+        } catch (unlinkError) {
+          console.error(`Erreur lors de la suppression du fichier temporaire : ${unlinkError.message}`);
+          // On ne lance pas l'erreur ici pour permettre la création du livre
+        }
+      }, 1000); // Délai de 1 seconde avant la suppression
     }
 
-    // Crée un nouvel objet livre
+    // Cherche la note donnée par l'utilisateur actuel dans le tableau des évaluations, s'il existe
+    const userRating = Array.isArray(bookObject.ratings)
+      ? bookObject.ratings.find((rating) => rating.userId === req.auth.userId)
+      : null;
+
+    // Si une note est trouvée pour cet utilisateur, assigne la note à 'userGrade'
+    // Sinon, assigne 'null' à 'userGrade' car l'utilisateur n'a pas encore noté le livre
+    const userGrade = userRating ? userRating.grade : null;
+
+    // Crée un nouvel objet livre avec les données reçues
     const book = new Book({
       title: bookObject.title,
       author: bookObject.author,
@@ -87,14 +93,112 @@ exports.createBook = async (req, res) => {
       userId: req.auth.userId,
     });
 
-    console.log("Book object to save:", book);
-
-    // Sauvegarde le livre
+    // Sauvegarde le nouveau livre dans la base de données
     const newBook = await book.save();
+
+    // Retourne une réponse avec le livre créé
     res.status(201).json(newBook);
   } catch (error) {
-    console.error("Error during book creation:", error.message);
+    console.error("Erreur lors de la création du livre:", error.message);
     res.status(400).json({ message: error.message });
+  }
+};
+
+
+//---------- Modifie un livre existant
+exports.modifyBook = async (req, res) => {
+  try {
+    console.log("Requête reçue pour modification:", req.body);
+    if (req.file) {
+      console.log("Fichier reçu pour modification:", req.file);
+    }
+
+    // Récupère le livre à modifier en utilisant l'ID de la requête
+    console.log("Recherche du livre à modifier avec ID:", req.params.id);
+    const book = await Book.findOne({ _id: req.params.id });
+    console.log("Livre trouvé pour modification:", book);
+
+    // Vérifie que l'utilisateur authentifié est bien celui qui a créé le livre
+    if (book.userId != req.auth.userId) {
+      console.log("Utilisateur non autorisé à modifier le livre.");
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    let bookObject;
+
+    // Si un nouveau fichier (image) est inclus dans la requête
+    if (req.file) {
+      console.log("Début de la compression de l'image:", req.file.path);
+      const oldFilename = path.basename(book.imageUrl);
+      const outputPath = `images/compressed-${req.file.filename}.webp`;
+
+      // Traite et compresse l'image reçue
+      await sharp(req.file.path)
+        .resize(300) // Redimensionne l'image
+        .webp({ quality: 80 }) // Compression WebP
+        .toFile(outputPath);
+      console.log("Compression terminée. Image sauvegardée:", outputPath);
+
+      // Crée un objet contenant les nouvelles données du livre, avec la nouvelle URL d'image
+      bookObject = {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get("host")}/${outputPath}`,
+      };
+      console.log("Nouveau bookObject avec image compressée:", bookObject);
+
+      // Délai pour s'assurer que le fichier non compressé est complètement disponible pour suppression
+      await delay(1000);
+      console.log("Tentative de suppression du fichier temporaire:", req.file.path);
+
+      try {
+        // Supprime l'image originale non compressée
+        await fs.unlink(req.file.path);
+        console.log("Fichier temporaire supprimé avec succès.");
+      } catch (error) {
+        console.error("Erreur lors de la suppression du fichier temporaire:", error.message);
+      }
+
+      // Délai supplémentaire avant de supprimer l'ancienne image
+      await delay(1000);
+
+      // Définit le chemin de l'ancienne image
+      const oldFilePath = `images/${oldFilename}`;
+      console.log("Tentative de suppression de l'ancienne image:", oldFilePath);
+
+      try {
+        // Vérifie si le fichier existe avant d'essayer de le supprimer
+        await fs.stat(oldFilePath);
+        await fs.unlink(oldFilePath);
+        console.log("Ancienne image supprimée avec succès:", oldFilePath);
+      } catch (error) {
+        if (error.code === "ENOENT") {
+          console.log("L'ancienne image n'existe pas :", oldFilePath);
+        } else {
+          console.error("Erreur lors de la suppression de l'ancienne image :", error.message);
+        }
+      }
+    } else {
+      // Si aucune nouvelle image n'est fournie, utilise les données du livre telles quelles
+      bookObject = { ...req.body };
+      console.log("Aucune nouvelle image fournie. Utilisation des données du livre telles quelles.");
+    }
+
+    // Supprime le champ _userId pour éviter toute modification non autorisée
+    delete bookObject._userId;
+
+    // Met à jour le livre avec les nouvelles données dans la base de données
+    console.log("Mise à jour du livre dans la base de données.");
+    await Book.updateOne(
+      { _id: req.params.id },
+      { ...bookObject, _id: req.params.id }
+    );
+
+    // Répond avec un message de succès
+    res.status(200).json({ message: "Livre modifié!" });
+  } catch (error) {
+    // Logue l'erreur en cas de problème et répond avec un message d'erreur
+    console.error("Erreur lors de la modification du livre:", error.message);
+    res.status(400).json({ error: error.message });
   }
 };
 
@@ -111,101 +215,6 @@ exports.getOneBook = async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
-  }
-};
-
-
-// Fonction pour ajouter un délai
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-//---------- Modifie un livre existant
-exports.modifyBook = async (req, res) => {
-  try {
-    // Log des données de la requête pour débogage
-    console.log("Requête reçue:", req.body);
-    if (req.file) {
-      console.log("Fichier reçu:", req.file);
-    }
-
-    // Récupère le livre à modifier en utilisant l'ID de la requête
-    const book = await Book.findOne({ _id: req.params.id });
-
-    // Vérifie que l'utilisateur authentifié est bien celui qui a créé le livre
-    if (book.userId != req.auth.userId) {
-      return res.status(401).json({ message: "Not authorized" });
-    }
-
-    let bookObject;
-
-    // Si un nouveau fichier (image) est inclus dans la requête
-    if (req.file) {
-      // Récupère le nom du fichier de l'image existante à partir de l'URL
-      const oldFilename = path.basename(book.imageUrl);
-      // Définit le chemin de sortie pour l'image compressée
-      const outputPath = `images/compressed-${req.file.filename}.webp`;
-
-      // Traite et compresse l'image reçue
-      await sharp(req.file.path)
-        .resize(300) // Redimensionne l'image
-        .webp({ quality: 80 }) // Compression WebP
-        .toFile(outputPath);
-
-      // Crée un objet contenant les nouvelles données du livre, avec la nouvelle URL d'image
-      bookObject = {
-        ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get("host")}/${outputPath}`,
-      };
-
-      // Délai pour s'assurer que le fichier non compressé est complètement disponible pour suppression
-      await delay(1000);
-
-      try {
-        // Supprime l'image originale non compressée
-        await safeUnlink(req.file.path);
-        } catch (error) {
-          console.error(`Failed to delete original image at ${req.file.path}:`, error.message);
-        }
-      // Délai supplémentaire avant de supprimer l'ancienne image
-      await delay(1000);
-
-      // Définit le chemin de l'ancienne image
-      const oldFilePath = `images/${oldFilename}`;
-      try {
-        // Vérifie si le fichier existe avant d'essayer de le supprimer
-        if (await fs.stat(oldFilePath)) {
-          // Supprime l'ancienne image
-          await safeUnlink(oldFilePath);
-          console.log(`Ancienne image supprimée : ${oldFilePath}`);
-        }
-      } catch (error) {
-        // Si le fichier n'existe pas, le logue comme tel
-        if (error.code === "ENOENT") {
-          console.log("L'ancienne image n'existe pas :", oldFilePath);
-        } else {
-          // Logue toute autre erreur lors de la suppression de l'ancienne image
-          console.error("Erreur lors de la suppression de l'ancienne image :", error);
-        }
-      }
-    } else {
-      // Si aucune nouvelle image n'est fournie, utilise les données du livre telles quelles
-      bookObject = { ...req.body };
-    }
-
-    // Supprime le champ _userId pour éviter toute modification non autorisée
-    delete bookObject._userId;
-
-    // Met à jour le livre avec les nouvelles données dans la base de données
-    await Book.updateOne(
-      { _id: req.params.id },
-      { ...bookObject, _id: req.params.id }
-    );
-
-    // Répond avec un message de succès
-    res.status(200).json({ message: "Livre modifié!" });
-  } catch (error) {
-    // Logue l'erreur en cas de problème et répond avec un message d'erreur
-    console.error("Erreur survenue:", error);
-    res.status(400).json({ error });
   }
 };
 
@@ -229,7 +238,7 @@ exports.deleteBook = async (req, res) => {
     // Supprime l'image associée si elle existe
     if (book.imageUrl) {
       const filename = book.imageUrl.split("/images/")[1];
-      await safeUnlink(`images/${filename}`);
+      await fs.unlink(`images/${filename}`);
     }
 
     // Supprime le livre de la base de données
